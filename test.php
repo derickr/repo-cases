f5ddd89ec7efc160fa9a0900e213dd2000729263
George Peter Banyard <girgias@php.net>
2022-12-09 10:45:44 +0000

    Add trmpoline

--- test.php
+++ test.php
@@ -4,6 +4,15 @@ require 'vendor/autoload.php';
 DG\BypassFinals::enable();
 DG\BypassFinals::setCacheDirectory(__DIR__ . '/cache');
 
+class TrampolineTest {
+    public function __call(string $name, array $arguments) {
+        echo 'Trampoline for ', $name, PHP_EOL;
+        return test(...$arguments);
+    }
+}
+$o = new TrampolineTest();
+$callback = [$o, 'trampoline'];
+
 function shutdown_handler()
 {
 	include 'final-class.php';
